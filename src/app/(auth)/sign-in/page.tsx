46bfeb27b251e191203c913f12aa2a248a20b194
hnsia <44498178+hnsia@users.noreply.github.com>
2023-07-20 22:22:56 +0800

    Add back icon to home in sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import SignIn from "@/components/SignIn";
 import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
+import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
 const page: React.FC = () => {
@@ -14,6 +15,7 @@ const page: React.FC = () => {
             "self-start -mt-20"
           )}
         >
+          <ChevronLeft className="mr-2 h-4 w-4" />
           Home
         </Link>
 
