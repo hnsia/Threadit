110120f839811321085bb13fef6a6e8f635c2af0
hnsia <44498178+hnsia@users.noreply.github.com>
2023-08-05 16:16:44 +0800

    Implement voting functionality part 2

--- src/app/api/subthreadit/post/vote/route.ts
+++ src/app/api/subthreadit/post/vote/route.ts
@@ -1,12 +1,14 @@
 import { CACHE_AFTER_UPVOTES } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
 import { PostVoteValidator } from "@/lib/validators/vote";
 import { CachedPost } from "@/types/redis";
+import { z } from "zod";
 
 export async function PATCH(req: Request) {
   try {
-    const body = req.json();
+    const body = await req.json();
 
     const { postId, voteType } = PostVoteValidator.parse(body);
 
@@ -79,7 +81,51 @@ export async function PATCH(req: Request) {
           currentVote: voteType,
           createdAt: post.createdAt,
         };
+
+        await redis.hset(`post:${postId}`, cachePayload);
       }
+
+      return new Response("Ok");
     }
-  } catch (error) {}
+
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+    // recount the votes
+    const voteAmount = post.votes.reduce((acc, vote) => {
+      if (vote.type === "UP") return acc + 1;
+      if (vote.type === "DOWN") return acc - 1;
+
+      return acc;
+    }, 0);
+
+    if (voteAmount >= CACHE_AFTER_UPVOTES) {
+      const cachePayload: CachedPost = {
+        authorUsername: post.author.username ?? "",
+        content: JSON.stringify(post.content),
+        id: post.id,
+        title: post.title,
+        currentVote: voteType,
+        createdAt: post.createdAt,
+      };
+
+      await redis.hset(`post:${postId}`, cachePayload);
+    }
+
+    return new Response("Ok");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed.", { status: 422 });
+    }
+
+    return new Response(
+      "Could not register your vote, please try again later.",
+      { status: 500 }
+    );
+  }
 }
