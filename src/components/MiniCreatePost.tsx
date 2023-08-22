0c81a07172b1479e6dea88e6e380ce1f9d8d5e2b
hnsia <44498178+hnsia@users.noreply.github.com>
2023-08-23 01:37:29 +0800

    Fix avatar css

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -26,7 +26,7 @@ const MiniCreatePost: React.FC<MiniCreatePostProps> = ({ session }) => {
             }}
           />
 
-          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
+          <span className="absolute overflow-y-clip bottom-6 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
         </div>
 
         <Input
